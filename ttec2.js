db8808f897a79ee26bc7d6e79ec515723b1ee337
Lee Clements <clemouk@users.noreply.github.com>
2024-03-25 16:24:58 +0100

    Updated url parsing.

--- ttec2.js
+++ ttec2.js
@@ -15,44 +15,51 @@ $(document).ready(function() {
 
 
 function launchGenesys() {
-    console.log('Preparing Genesys Widget...');
+  console.log('Preparing Genesys Widget...');
 
-    (function getDeviceType() {
-      if($.browser.mobile==true) {
-        deviceType = 'Mobile Device'
-      } else {
-        deviceType = 'Desktop'
+  (function getDeviceType() {
+    if($.browser.mobile==true) {
+      deviceType = 'Mobile Device'
+    } else {
+      deviceType = 'Desktop'
+    }
+  })();
+
+
+  var _originUrl=$("#launchKeyURL option:selected").text();
+
+  if(_originUrl.includes('workplace')){
+    _originUrl += $('input[name="schemeId"]').val();
+  }
+
+  Genesys('command', 'Database.set', {
+    messaging: {
+        customAttributes: {
+            productTemplate: $('input[name="productTemplate"]').val(),
+            schemeNumber:  $('input[name="schemeId"]').val(),
+            productType: $("#productType option:selected").text(),
+            policyNumber: $('input[name="policyId"]').val(),
+            valuationClass: $('input[name="valueClass"]').val(),
+            originUrl: _originUrl,
+            //deepLinkType: $("#deepLinkType option:selected").text(),
+            deepLinkId: $('input[name="deepLinkId"]').val(),
+            target:  $('input[name="target"]').val(),
+            syndicationId: $('input[name="syndicationId"]').val(),              
+            browserType: $.browser.platform,
+            browserVersion: $.browser.version,      
+            deviceType: deviceType      
+        },
+    },
+})
+
+  $('#wizardContainer').fadeOut();
+  // for the  demo, just open the messenger
+    Genesys('command','Messenger.open',{},
+      function (o) {},
+      function (o) {
+        Genesys('command', 'Messenger.close')
       }
-    })();
-
-    Genesys('command', 'Database.set', {
-      messaging: {
-          customAttributes: {
-              productTemplate: $('input[name="productTemplate"]').val(),
-              schemeNumber:  $('input[name="schemeId"]').val(),
-              productType: $("#productType option:selected").text(),
-              policyNumber: $('input[name="policyId"]').val(),
-              valuationClass: $('input[name="valueClass"]').val(),
-              originUrl: $("#launchKeyURL option:selected").text(),
-              //deepLinkType: $("#deepLinkType option:selected").text(),
-              deepLinkId: $('input[name="deepLinkId"]').val(),
-              target:  $('input[name="target"]').val(),
-              syndicationId: $('input[name="syndicationId"]').val(),              
-              browserType: $.browser.platform,
-              browserVersion: $.browser.version,      
-              deviceType: deviceType      
-          },
-      },
-  })
-
-    $('#wizardContainer').fadeOut();
-    // for the  demo, just open the messenger
-      Genesys('command','Messenger.open',{},
-        function (o) {},
-        function (o) {
-          Genesys('command', 'Messenger.close')
-        }
-      )
+    )
 };
 
 function toggleMessenger(){
