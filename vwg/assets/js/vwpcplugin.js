a1928cdc136ca44108b86e069bd5d07ae06aa345
Lee Clements <clemouk@users.noreply.github.com>
2024-10-10 17:12:15 +0200

    Updated VWPC to resend database parameters on new conversation

--- vwg/assets/js/vwpcplugin.js
+++ vwg/assets/js/vwpcplugin.js
@@ -10,6 +10,44 @@ if (surveyDone == null || surveyDone == undefined) {
   surveyDone = 'false'
 }
 
+// subscribe to ready event
+Genesys('subscribe', 'Messenger.ready', function () {
+  console.log('setting db params');
+
+  wireEvents();
+});
+
+// receive disconnected event
+Genesys('subscribe', 'MessagingService.conversationDisconnected', function () {
+
+  if (!loaded) {
+    loaded = true
+    conversationEnd = 'true'
+    localStorage.setItem('conversationEnd', 'true')
+    console.log('end of conversation')
+    console.log(conversationEnd)
+    console.log(surveyDone)
+    if (surveyDone == 'false') {
+      localStorage.setItem('surveyDone', 'true')
+      console.log('Start Survey')
+          Genesys('command', 'MessagingService.sendMessage', {
+            message: 'How did we do?',
+          })
+    }
+  }
+})
+
+// receive connected event
+Genesys('subscribe', 'Conversations.started', function () {
+  console.log('new conversation')
+  setWidgetParams()
+  conversationEnd = 'false'
+  surveyDone = 'false'
+  loaded = false
+  localStorage.setItem('conversationEnd', 'false')
+  localStorage.setItem('surveyDone', 'false')
+})
+
 function wireEvents(){
   console.log('wireEvents - begin');
 
@@ -51,12 +89,7 @@ function wireEvents(){
   console.log('wireEvents - end');
 }
 
-// subscribe to ready event
-Genesys('subscribe', 'Messenger.ready', function () {
-  console.log('setting db params');
-
-  wireEvents();
-
+function setWidgetParams() {
   Genesys('command', 'Database.set', {
     messaging: {
         customAttributes: {
@@ -64,39 +97,7 @@ Genesys('subscribe', 'Messenger.ready', function () {
         },
     },
   })
-});
-
-
-// receive disconnected event
-Genesys('subscribe', 'MessagingService.conversationDisconnected', function () {
-
-  if (!loaded) {
-    loaded = true
-    conversationEnd = 'true'
-    localStorage.setItem('conversationEnd', 'true')
-    console.log('end of conversation')
-    console.log(conversationEnd)
-    console.log(surveyDone)
-    if (surveyDone == 'false') {
-      localStorage.setItem('surveyDone', 'true')
-      console.log('Start Survey')
-          Genesys('command', 'MessagingService.sendMessage', {
-            message: 'How did we do?',
-          })
-    }
-  }
-})
-
-// receive connected event
-Genesys('subscribe', 'Conversations.started', function () {
-  console.log('new conversation')
-  conversationEnd = 'false'
-  surveyDone = 'false'
-  loaded = false
-  localStorage.setItem('conversationEnd', 'false')
-  localStorage.setItem('surveyDone', 'false')
-})
-
+}
 
 function toggleMessenger(){
   Genesys("command", "Messenger.open", {},
