894534dc305ab67d7cd1528a74e232073950c7d0
Lee Clements <clemouk@users.noreply.github.com>
2024-10-11 17:46:39 +0200

    REstored VWPC to original code

--- vwg/assets/js/vwpcplugin.js
+++ vwg/assets/js/vwpcplugin.js
@@ -2,7 +2,6 @@
 let conversationEnd = localStorage.getItem('conversationEnd')
 let surveyDone = localStorage.getItem('surveyDone')
 let loaded = false
-let conversationReset = false
 
 if (conversationEnd == null || conversationEnd == undefined) {
   conversationEnd = 'false'
@@ -11,16 +10,65 @@ if (surveyDone == null || surveyDone == undefined) {
   surveyDone = 'false'
 }
 
+function wireEvents(){
+  console.log('wireEvents - begin');
+
+      // subsribe to close widget event
+      console.log('READY: subscribing to open event...');
+
+
+      console.log('READY: subscribing to conversationCleared event...');
+      Genesys('subscribe', 'MessagingService.conconversationClearedversationCleared', function(){
+        console.log('MessagingService. event invoked');
+      });
+
+      let x = document.getElementById("myAudio");
+
+      Genesys("subscribe", "Messenger.opened", function(){
+        console.log('Messenger.open event invoked');
+        messengerOpen = true;
+      });
+
+      Genesys("subscribe", "Messenger.closed", function(){
+        messengerOpen = false;
+      });
+
+      console.log('READY: subscribing to messagesReceived event...');
+      Genesys("subscribe", "MessagingService.messagesReceived", function({ data }) {
+        console.log(data);
+        if(messengerOpen==false) {
+          x.play();
+          Genesys('command','Messenger.open',{},
+            function (o) {},
+            function (o) {
+              Genesys('command', 'Messenger.close');        
+            }
+          )
+        }; 
+      })
+
+
+  console.log('wireEvents - end');
+}
+
 // subscribe to ready event
 Genesys('subscribe', 'Messenger.ready', function () {
-  console.log('Messenger.Ready event');
+  console.log('setting db params');
+
   wireEvents();
+
+  Genesys('command', 'Database.set', {
+    messaging: {
+        customAttributes: {
+            TargetBrand: "Audi"
+        },
+    },
+  })
 });
+
 
 // receive disconnected event
 Genesys('subscribe', 'MessagingService.conversationDisconnected', function () {
-
-  setWidgetParams();
 
   if (!loaded) {
     loaded = true
@@ -31,7 +79,7 @@ Genesys('subscribe', 'MessagingService.conversationDisconnected', function () {
     console.log(surveyDone)
     if (surveyDone == 'false') {
       localStorage.setItem('surveyDone', 'true')
-      console.log('COMMAND: Start Survey')
+      console.log('Start Survey')
           Genesys('command', 'MessagingService.sendMessage', {
             message: 'How did we do?',
           })
@@ -41,78 +89,14 @@ Genesys('subscribe', 'MessagingService.conversationDisconnected', function () {
 
 // receive connected event
 Genesys('subscribe', 'Conversations.started', function () {
-  console.log('new conversation');
-  setWidgetParams();
-  conversationEnd = 'false';
-  surveyDone = 'false';
-  loaded = false;
-  localStorage.setItem('conversationEnd', 'false');
-  localStorage.setItem('surveyDone', 'false');
+  console.log('new conversation')
+  conversationEnd = 'false'
+  surveyDone = 'false'
+  loaded = false
+  localStorage.setItem('conversationEnd', 'false')
+  localStorage.setItem('surveyDone', 'false')
 })
 
-function wireEvents(){
-  console.log('wireEvents - begin');
-
-  console.log('READY: subscribing to conversationCleared event...');
-  Genesys('subscribe', 'MessagingService.conversationCleared', function(){
-    console.log('MessagingService.conversationCleared event invoked');
-    conversationReset = true;
-    wireEvents();
-  });
-
-  let x = document.getElementById("myAudio");
-
-  // subsribe to close widget event
-  console.log('READY: subscribing to open event...');
-  Genesys("subscribe", "Messenger.opened", function(){
-    console.log('Messenger.open event invoked');
-    messengerOpen = true;
-
-    if(conversationReset==true) {
-      Genesys("command", "MessagingService.startConversation",
-        {},
-        function() {
-            /*fulfilled callback*/
-           
-        },
-        function() {
-            /*rejected callback*/
-            console.log(e);
-          
-        });
-    }
-    conversationReset=false;
-  });
-
-  // subsribe to close widget event
-  console.log('READY: subscribing to closed event...');
-  Genesys("subscribe", "Messenger.closed", function(){
-    messengerOpen = false;
-  });
-
-  console.log('READY: subscribing to messagesReceived event...');
-  Genesys("subscribe", "MessagingService.messagesReceived", function({ data }) {
-    //console.log(data);
-    if(messengerOpen==false) {
-      x.play();
-      toggleMessenger();
-    }; 
-  })
-
-  setWidgetParams();
-  console.log('wireEvents - end');
-}
-
-function setWidgetParams() {
-  console.log('COMMAND: Set customAttributes');
-  Genesys('command', 'Database.set', {
-    messaging: {
-        customAttributes: {
-            TargetBrand: "VWPC"
-        },
-    },
-  })
-}
 
 function toggleMessenger(){
   Genesys("command", "Messenger.open", {},
