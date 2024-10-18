c696da81a1d2db3d3ac12947042779f85d9685a8
Lee Clements <clemouk@users.noreply.github.com>
2024-10-18 16:24:42 +0200

    as

--- vwg/assets/js/vwpcplugin.js
+++ vwg/assets/js/vwpcplugin.js
@@ -13,10 +13,7 @@ if (surveyDone == null || surveyDone == undefined) {
 };
 
 function wireEvents(){
-  console.log('wireEvents - begin');
-
   // subsribe to close widget event
-  console.log('READY: subscribing to conversationCleared event...');
   Genesys('subscribe', 'MessagingService.conversationCleared', function(){
     // Need to reset the conversationState so that a survey can be done if a new conversation starts
     localStorage.setItem('_ttecConversationState', 'NEW');
@@ -33,11 +30,9 @@ function wireEvents(){
   let x = document.getElementById("myAudio");
 
   Genesys("subscribe", "Messenger.opened", function(){
-    console.log('Messenger.open event invoked');
     messengerOpen = true;
 
     if(localStorage.getItem('_ttecConversationState')=='SURVEY_COMPLETED') {
-      console.log('Resetting widgets params - TargetBrand: VWPC');
       Genesys('command', 'Database.set', {
         messaging: {
             customAttributes: {
@@ -50,11 +45,10 @@ function wireEvents(){
   });
 
   Genesys("subscribe", "Messenger.closed", function(){
-    console.log('Messenger.closed event invoked');
+    ('Messenger.closed event invoked');
     messengerOpen = false;
   });
 
-  console.log('READY: subscribing to messagesReceived event...');
   Genesys("subscribe", "MessagingService.messagesReceived", function({ data }) {
 
     if((data.messages[0].type=="Text" || data.messages[0].type=="Structured") && (data.messages[0].direction=="Outbound" && data.messages[0].originatingEntity=="Bot"))
@@ -70,12 +64,10 @@ function wireEvents(){
 
       if(messageContent.indexOf("*Question ")>-1) { 
         localStorage.setItem('_ttecConversationState', 'IN_SURVEY');
-        console.log('_ttecConversationState = IN_SURVEY')
       } 
       else if(messageContent=="Hello. I'm your Volkswagen Digital Assistant.") 
         {
           localStorage.setItem('_ttecConversationState', 'NEW');
-          console.log('new conversation')
           conversationEnd = 'false'
           surveyDone = 'false'
           loaded = false
@@ -91,7 +83,6 @@ function wireEvents(){
       else if(messageContent=="Thanks for submitting your feedback.") 
       {
         localStorage.setItem('_ttecConversationState', 'SURVEY_COMPLETED');
-        console.log('_ttecConversationState = SURVEY_COMPLETED')
         Genesys('command', 'Database.set', {
           messaging: {
               customAttributes: {
@@ -101,25 +92,19 @@ function wireEvents(){
         })
       } else {
         localStorage.setItem('_ttecConversationState', 'IN_PROGRESS');
-        console.log('_ttecConversationState = IN_PROGRESS');
       }
     };
 
-    //console.log(data);
     if(messengerOpen==false) {
       x.play();
       toggleMessenger();
     };
   })
 
-
-
-  console.log('wireEvents - end');
 }
 
 // subscribe to ready event
 Genesys('subscribe', 'Messenger.ready', function () {
-  console.log('setting db params');
 
   wireEvents();
 
@@ -137,7 +122,6 @@ Genesys('subscribe', 'Messenger.ready', function () {
 // receive disconnected event
 Genesys('subscribe', 'MessagingService.conversationDisconnected', function () {
 
-  console.log('disconnected event');
   //add localstorage flags to indicate how many times and also time
 
   if (!loaded) {
@@ -147,7 +131,6 @@ Genesys('subscribe', 'MessagingService.conversationDisconnected', function () {
     localStorage.setItem('conversationEnd', 'true')
     if (surveyDone == 'false') {
       localStorage.setItem('surveyDone', 'true')
-      console.log('Start Survey')
       Genesys('command', 'MessagingService.sendMessage', {
         message: 'How did we do?',
       })
@@ -157,7 +140,6 @@ Genesys('subscribe', 'MessagingService.conversationDisconnected', function () {
 
 // receive connected event
 Genesys('subscribe', 'Conversations.started', function () {
-  console.log('new conversation')
   conversationEnd = 'false'
   surveyDone = 'false'
   loaded = false
